56d09a33290140c4304e4b51b10ef85ea539624e
Johannnes Güttl <johannnes.guettl@gmail.com>
2025-05-29 11:13:13 +0200

    initial commit

---
+++ Test.py
@@ -0,0 +1,15 @@
+from random import uniform
+sum = 0
+Nam = []
+zähl = 0
+for i in range(1, 10):
+     fl = uniform(0, 10)
+     if fl < 3:
+        print('Zufallsvariable ist kleiner 3:   ')
+        zähl += 1
+     Nam = str(round(fl, 2))
+     sum += fl 
+     print(Nam, ' ')
+
+print('')
+print('Summe = ', sum, '    Anzahl der Zahlen < 3 = ', zähl)
